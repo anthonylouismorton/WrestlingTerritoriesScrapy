16d394e358d30f1662213ec97528256db688f39d
anthonymorton <anthonymorton760@gmail.com>
2022-05-20 22:45:29 -0700

    need to fix if statement for WWW as it will not create and object for wrestlers with no social media

--- wrestlingterritories/items.py
+++ wrestlingterritories/items.py
@@ -4,9 +4,56 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
+
+
+def remove_slash(value):
+    return value.replace("\\", "").strip()
 
 
 class WrestlingterritoriesItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    Wrestler = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Currentgimmick = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Age = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Promotion = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Brand = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Birthplace = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Gender = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Height = scrapy.Field(input_processor=MapCompose(
+        remove_tags, remove_slash), output_processor=TakeFirst())
+    Weight = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Backgroundinsports = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    SocialMedia = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Alteregos = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Roles = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Beginningofinringcareer = scrapy.Field(
+        input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
+    Endofinringcareer = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Inringexperience = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Wrestlingstyle = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Trainer = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Nicknames = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    Signaturemoves = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
+    ActiveRoles = scrapy.Field(input_processor=MapCompose(
+        remove_tags), output_processor=TakeFirst())
--- wrestlingterritories/spiders/wrestler.py
+++ wrestlingterritories/spiders/wrestler.py
@@ -1,4 +1,6 @@
 import scrapy
+from wrestlingterritories.items import WrestlingterritoriesItem
+
 
 class WrestlerSpider(scrapy.Spider):
     name = 'wrestler'
@@ -8,27 +10,61 @@ class WrestlerSpider(scrapy.Spider):
         for wrestler in response.css('tr.TRow1, tr.TRow2'):
             URL = wrestler.css('td.TCol.TColSeparator a::attr(href)').get()
             wrestlerURL = f'https://www.cagematch.net/{URL}'
+            # item['WrestlerName'] = wrestler.css('a::text').get()
+            # yield item
             yield response.follow(wrestlerURL, callback=self.parse2)
+
         pageNumber = 100
         next_page_link = f'https://www.cagematch.net/?id=2&view=workers&s={pageNumber}'
         while pageNumber < 200:
             yield response.follow(next_page_link, callback=self.parse)
-            pageNumber +=100
+            pageNumber += 100
             next_page_link = f'https://www.cagematch.net/?id=2&view=workers&s={pageNumber}'
-    def parse2(self, response):
-        for row in response.css('div.InformationBoxRow'):
-            title = row.css('div.InformationBoxTitle::text').get()
-            content = row.css('div.InformationBoxContents::text')
 
+    def parse2(self, response):
+        item = WrestlingterritoriesItem()
+        item['Wrestler'] = response.css('h1.TextHeader::text').get()
+        aka = response.css('h2.TextSubHeader::text').get().replace(
+            'Also known as ', '')
+        akalist = aka.split(',')
+        item['Alteregos'] = akalist
+        for row in response.css('div.InformationBoxRow'):
+            title = row.css(
+                'div.InformationBoxTitle::text').get().replace(':', '')
+            title2 = title.replace(' ', '')
+            title3 = title2.replace('-', '')
+            content = row.css('div.InformationBoxContents::text')
+            if title3 == 'WWW':
+                title3 = 'SocialMedia'
+                accountList = []
+                for account in response.css('ul.noList li'):
+                    accountList.append(account.css('li a').attrib['href'])
+                item[title3] = accountList
             if content.get() is not None:
-                yield{
-                    f'{title}': content.get()
-                }
+                if title3 == 'Alteregos':
+                    pass
+                else:
+                    item[title3] = content.get()
             else:
-                yield{
-                    f'{title}': row.css('div.InformationBoxContents a::text').get()
-                }
-            # wrestlerLink = wrestlerURL.css('a').attrib['href']
-            # yield{
-            #     'Wrestler': wrestler.css('a::text').get()
-            # }
+                # if title3 == 'WWW':
+                #     title3 = 'SocialMedia'
+                #     accountList = []
+                #     for account in response.css('ul.noList li'):
+                #         accountList.append(account.css('li a').attrib['href'])
+                #     item[title3] = accountList
+                # else:
+                item[title3] = row.css(
+                    'div.InformationBoxContents a::text').get()
+        yield item
+        # if content.get() is not None:
+        #     yield{
+        #         f'{title}': content.get()
+        #     }
+        # else:
+        #     yield{
+        #         f'{title}': row.css('div.InformationBoxContents a::text').get()
+        #     }
+        # wrestlerLink = wrestlerURL.css('a').attrib['href']
+        # yield{
+        #     'Wrestler': wrestler.css('a::text').get()
+        # }
