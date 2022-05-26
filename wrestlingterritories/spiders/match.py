584377af099bdd8db3b05fd129e34049b6f2895b
anthonymorton <anthonymorton760@gmail.com>
2022-05-25 21:36:36 -0700

    able to get the main information about each event. Need to loop through each match for each event to get match information. Will possibly bet able to .getall() matchType, matchResults, etc. Could have an issue with match results as there it will be difficult to seperate wrestlers and who won a match. May need to just find match results by getting information from each wrestler.

--- wrestlingterritories/spiders/match.py
+++ wrestlingterritories/spiders/match.py
@@ -10,17 +10,18 @@ class MatchSpider(scrapy.Spider):
         'https://www.cagematch.net/en/?id=8&view=promotions&region=Amerika&status=&name=&location=&minRating=&maxRating=&minVotes=&maxVotes=']
 
     def parse(self, response):
-        for promotion in response.css('tr.TRow1, tr.TRow2'):
-            URL = promotion.css('td.TCol.TColSeparator a::attr(href)').get()
-            yield response.follow(URL, callback=self.parse2)
-            pageNumber = 0
-            next_page_link = f'https://www.cagematch.net/en/?id=8&view=promotions&region=Amerika&s={pageNumber}'
+        # commented out lines 14 - 23 for testing purposes
+        # for promotion in response.css('tr.TRow1, tr.TRow2'):
+        #     URL = promotion.css('td.TCol.TColSeparator a::attr(href)').get()
+        #     yield response.follow(URL, callback=self.parse2)
+        #     pageNumber = 0
+        #     next_page_link = f'https://www.cagematch.net/en/?id=8&view=promotions&region=Amerika&s={pageNumber}'
         # while pageNumber == 0:
         #     yield response.follow(next_page_link, callback=self.parse)
         #     pageNumber += 100
 
         #     next_page_link = f'https://www.cagematch.net/en/?id=8&view=promotions&region=Amerika&s={pageNumber}'
-        # yield response.follow('https://www.cagematch.net/en/?id=8&view=promotions&region=Amerika&s=0', callback=self.parse2)
+        yield response.follow('https://www.cagematch.net/en/?id=8&nr=1', callback=self.parse2)
 
     def parse2(self, response):
         # promotionName = response.css('h1.TextHeader::text').get().split(' (')
@@ -35,37 +36,41 @@ class MatchSpider(scrapy.Spider):
     def parse3(self, response):
         table = response.css('table.TBase.TableBorderColor')
         rows = table.css('tr')
-        print(rows)
-        # rows = table.css('tr.TRow1.TRowCard, tr.TRow2.TRowCard')
+        rows = table.css('tr.TRow1, tr.TRow2')
         for row in rows:
-            matchPage = row.css('td.TCol.TColSeparator a::attr(href)').get()
-            # print(matchPage)
-            yield response.follow(matchPage, callback = self.parse4)
+            matchPage = row.css('td.TCol.TColSeparator a::attr(href)').getall()
+            yield response.follow(matchPage[1], callback = self.parse4)
 
     def parse4(self, response):
+        header = response.css('h1.TextHeader::text').get()
+        print(header)
         item = WrestlingMatchItem()
         eventInfo = response.css('div.InformationBoxTable')
+        matchInfo = response.css('div.Matches')
         for row in eventInfo.css('div.InformationBoxRow'):
             title = row.css(
                 'div.InformationBoxTitle::text').get().replace(':', '')
             title2 = title.replace(' ', '')
             title3 = title2.replace('-', '')
-            print(title3)
             content = row.css('div.InformationBoxContents a::text, div.InformationBoxContents::text')
             if title3 == 'Nameoftheevent':
                 title3 = 'EventName'
-                item[title3] = content
+                print(title3)
+                item[title3] = content.get()
                 continue
             if title3 == 'TVstation/network':
                 title3 = 'TVStation'
-                item[title3] = content
+                item[title3] = content.get()
                 continue
             if title3 == 'Commentaryby':
                 title3 = 'Commentators'
-                item[title3] = content
+                item[title3] = content.get()
                 continue
-            # else:
-            #     item[title3] = content
+            if content.get() is not None:
+                item[title3] = content.get()
+        for match in matchInfo:
+            
+
         yield item
 
 
